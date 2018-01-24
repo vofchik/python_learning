47ed0dd232d5d5a2bfbd6c01c0e49c9b1d68908a
pva <pva@pvanb.tgn.abcp.ru>
2018-01-24 22:23:51 +0300

    exercise 2

--- exercise_02/task_02.py
+++ exercise_02/task_02.py
@@ -1,6 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Упражнение 1 
 # Задание 2
 
 # Программа, которая считывает два числа и выводит их сумму
+import re
+str = input('Не будь долбоебом, введи два целых числа через пробел: ')
+digits = str.split(' ')
+if len(digits) == 2 and bool(re.match('^[0-9\-][0-9]+$',digits[0])
+) and bool(re.match('^[0-9\-][0-9]+$',digits[1])):
+  print('{} + {} = {}'.format(digits[0],digits[1],int(digits[0]) + int(digits[1])))
+
