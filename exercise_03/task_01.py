3bf5b831ffa799ae53d0505b700a312b73472246
pva <pva@pvanb.tgn.abcp.ru>
2018-01-24 23:29:47 +0300

    exercise 3

--- exercise_03/task_01.py
+++ exercise_03/task_01.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-# Упражнение 1 
+# Упражнение 3 
 # Задание 1
 
 # Вычислить модуль числа без использования библиотечных функций.
+number = input('Введи число: ')
+try:
+  number = float(number)
+  if number < 0:
+     number = -number
+except:
+  print('Дружище, похоже ты ввел какую-то хуйню, а не число!')
+  exit()  
+print('Модуль числа {:.2f}'.format(number))
+
